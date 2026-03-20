55c39f981cb1ead88ad294463657108a6a01328d
Christopher-Lamb <70704179+Christopher-Lamb@users.noreply.github.com>
2026-03-20 16:00:26 -0400

    Updated Scopes

--- app/api/square/oauth/start/route.ts
+++ app/api/square/oauth/start/route.ts
@@ -6,6 +6,16 @@ export async function GET() {
   const clientId = process.env.SQUARE_APP_ID!;
   const redirectUri = process.env.SQUARE_REDIRECT_URI!;
 
-  const url = `https://connect.squareup.com/oauth2/authorize?client_id=${clientId}&response_type=code&scope=PAYMENTS_WRITE+ORDERS_WRITE+MERCHANT_PROFILE_READ+ITEMS_READ&redirect_uri=${redirectUri}&state=${clientId}`;
+  const scopes = [
+    "PAYMENTS_WRITE",
+    "ORDERS_WRITE",
+    "ORDERS_READ",
+    "ITEMS_READ",
+    "MERCHANT_PROFILE_READ",
+    "CUSTOMERS_READ",
+    "CUSTOMERS_WRITE",
+  ].join("+");
+  
+  const url = `https://connect.squareup.com/oauth2/authorize?client_id=${clientId}&response_type=code&scope=${scopes}&redirect_uri=${redirectUri}&state=${clientId}`;
   return NextResponse.redirect(url);
 }
