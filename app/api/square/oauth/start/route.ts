da70ae1f131765eb855a915257ffab91cea09ab5
Christopher-Lamb <70704179+Christopher-Lamb@users.noreply.github.com>
2026-03-20 13:38:51 -0400

    Added MERCHANT_PROFILE_READ to square get route

--- app/api/square/oauth/start/route.ts
+++ app/api/square/oauth/start/route.ts
@@ -6,6 +6,6 @@ export async function GET() {
   const clientId = process.env.SQUARE_APP_ID!;
   const redirectUri = process.env.SQUARE_REDIRECT_URI!;
 
-  const url = `https://connect.squareup.com/oauth2/authorize?client_id=${clientId}&response_type=code&scope=PAYMENTS_WRITE+ORDERS_WRITE&redirect_uri=${redirectUri}&state=${clientId}`;
+  const url = `https://connect.squareup.com/oauth2/authorize?client_id=${clientId}&response_type=code&scope=PAYMENTS_WRITE+ORDERS_WRITE+MERCHANT_PROFILE_READ&redirect_uri=${redirectUri}&state=${clientId}`;
   return NextResponse.redirect(url);
 }
